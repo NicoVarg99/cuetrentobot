5093622fea6e05942f33d50cd7a2602c9839fddb
Nicola Salsotto <nicolasalsotto@gmail.com>
2018-11-29 23:04:41 +0100

    Basic telegram info

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-while true; do
-  bash "./parser.sh"
+# while true; do
+  # bash "./parser.sh"
   node "index.js"
-  echo "Sleeping for 1 minute"
-  sleep 60
-done
+  # echo "Sleeping for 1 minute"
+  # sleep 60
+# done
--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 var fs = require("fs");
 const TelegramBot = require('node-telegram-bot-api');
 const token =  fs.readFileSync('token', 'utf8').trim();
-//const execSync = require('child_process').execSync;
+const chatid =  fs.readFileSync('data/chatid', 'utf8').trim();
+const execSync = require('child_process').execSync;
 console.log("\n *START* \n");
-var newData = JSON.parse(fs.readFileSync("data/newData.json"));
-var oldData = JSON.parse(fs.readFileSync("data/oldData.json"));
-//code = execSync('./parser.sh');
 
- for (var i = 0; i < newData.length; i++) {
-   console.log("Current element:");
-   console.log("Lat: " + newData[i].lat);
-   console.log("Lng: " + newData[i].lng);
-   console.log("Tipo evento: " + newData[i].type);
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+bot.sendMessage(chatid, "Bot started");
+
+function checkUpdates() {
+  console.log("Downloading data...");
+  code = execSync('./parser.sh');
+  console.log("Checking...");
+  var newData = JSON.parse(fs.readFileSync("data/newData.json"));
+  var oldData = JSON.parse(fs.readFileSync("data/oldData.json"));
+
+  for (var i = 0; i < newData.length; i++) {
+   // console.log("Current element:");
+   // console.log("Lat: " + newData[i].lat);
+   // console.log("Lng: " + newData[i].lng);
+   // console.log("Tipo evento: " + newData[i].type);
 
    var found = false;
-   console.log("Searching...")
+   // console.log("Searching...")
    for (var j = 0; j < oldData.length; j++) {
      if (oldData[j].lat == newData[i].lat && oldData[j].lng == newData[i].lng && oldData[j].type == newData[i].type) {
        //Found
-       console.log("Found.\n");
+       //console.log("Found.\n");
        found = true;
      }
      //.firstSeen = new Date().getTime();
@@ -32,13 +41,26 @@ var oldData = JSON.parse(fs.readFileSync("data/oldData.json"));
        type: newData[i].type,
        firstSeen: firstSeen = new Date().getTime()
      });
+     console.log("New object:");
+     console.log("Lat: " + newData[i].lat);
+     console.log("Lng: " + newData[i].lng);
+     console.log("Tipo evento: " + newData[i].type);
      //TODO: Notify
+     if (newData[i].type == 115) {
+      message = "Evento 115 🚒\n" + "Lat: " + newData[i].lat + "\nLng: " + newData[i].lng;
+     } else {
+      message = "Evento 118 🚑\n" + "Lat: " + newData[i].lat + "\nLng: " + newData[i].lng;
+     }
+     bot.sendMessage(chatid, message);
    }
+  }
 
- }
+  //Save oldData to file
+  fs.writeFileSync('data/oldData.json', JSON.stringify(oldData));
+  console.log(oldData.length + " objects written to file. Restarting in 60 seconds.");
+  setTimeout(function() {checkUpdates()}, 60000);
+}
 
-//Save oldData to file
-fs.writeFileSync('data/oldData.json', JSON.stringify(oldData));
-console.log( oldData.length + " objects written to file.");
+checkUpdates();
 
-console.log("\n *EXIT* \n");
+//console.log("\n *EXIT* \n");
