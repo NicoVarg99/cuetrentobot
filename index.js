c7862e006aa01939cb232884c33d851a49477dd7
Nicola Salsotto <nicolasalsotto@gmail.com>
2018-11-29 23:48:06 +0100

    Add reverse geocoding

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const TelegramBot = require('node-telegram-bot-api');
 const token =  fs.readFileSync('token', 'utf8').trim();
 const chatid =  fs.readFileSync('data/chatid', 'utf8').trim();
 const execSync = require('child_process').execSync;
-console.log("\n *START* \n");
+var NodeGeocoder = require('node-geocoder');
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 bot.sendMessage(chatid, "Bot started");
+
+var gcoptions = {
+  provider: 'openstreetmap',
+  // Optional depending on the providers
+  httpAdapter: 'https', // Default
+  formatter: null         // 'gpx', 'string', ...
+};
+
+var geocoder = NodeGeocoder(gcoptions);
 
 function checkUpdates() {
   console.log("Downloading data...");
@@ -45,13 +54,24 @@ function checkUpdates() {
      console.log("Lat: " + newData[i].lat);
      console.log("Lng: " + newData[i].lng);
      console.log("Tipo evento: " + newData[i].type);
-     //TODO: Notify
+
+     //Notify
      if (newData[i].type == 115) {
-      message = "Evento 115 🚒\n" + "Lat: " + newData[i].lat + "\nLng: " + newData[i].lng;
+       message = "Evento 115 🚒\n";
      } else {
-      message = "Evento 118 🚑\n" + "Lat: " + newData[i].lat + "\nLng: " + newData[i].lng;
+       message = "Evento 118 🚑\n";
      }
-     bot.sendMessage(chatid, message);
+
+     geocoder.reverse({lat:46.0684761985, lon:11.1169754318}, function(err, res) {
+       if (err) {
+         console.log(err);
+         message += "Lat: " + newData[i].lat + "\nLng: " + newData[i].lng;
+       } else {
+         res = res[0];
+         message += res.formattedAddress;
+       }
+       bot.sendMessage(chatid, message);
+     });
    }
   }
 
@@ -62,5 +82,3 @@ function checkUpdates() {
 }
 
 checkUpdates();
-
-//console.log("\n *EXIT* \n");
